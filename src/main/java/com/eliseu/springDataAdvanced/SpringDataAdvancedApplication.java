c70297dc42cb254b802d4e9fa7e318344ba18230
eliseusbrito <eliseusbrito@gmail.com>
2022-08-21 11:58:15 -0300

    Criado projeto em start.spring.io

---
+++ src/main/java/com/eliseu/springDataAdvanced/SpringDataAdvancedApplication.java
@@ -0,0 +1,13 @@
+package com.eliseu.springDataAdvanced;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataAdvancedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataAdvancedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eliseu/springDataAdvanced/SpringDataAdvancedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eliseu.springDataAdvanced;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataAdvancedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
